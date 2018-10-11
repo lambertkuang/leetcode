5d4b98523a18ca315b0b5a3414362c653bdb98f1
Lambert <lambertkuang@gmail.com>
2018-10-10 22:48:23 -0700

    Format two sum problem

--- two_sum.py
+++ two_sum.py
@@ -1,21 +1,22 @@
-def two_sum(nums, target):
-    """
-    Given an array of integers, return indices of the two numbers such that they add up to a specific
-    target.
-    
-    You may assume that each input would have exactly one solution, and you may not use the same
-    element twice.
-    
-    Example:
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        Given an array of integers, return indices of the two numbers such that they add up to a specific
+        target.
         
+        You may assume that each input would have exactly one solution, and you may not use the same
+        element twice.
+        
+        Example:
+
         Given nums = [2, 7, 11, 15], target = 9,
-        
+
         Because nums[0] + nums[1] = 2 + 7 = 9,
         return [0, 1].
-    """ 
-    seen = {}
-    for index, n in enumerate(nums):
-        if seen.get(target - n) is not None:
-            return [seen[target - n], index]
-        seen[n] = index
-    return
+        """ 
+        seen = {}
+        for index, n in enumerate(nums):
+            if seen.get(target - n) is not None:
+                return [seen[target - n], index]
+            seen[n] = index
+        return
