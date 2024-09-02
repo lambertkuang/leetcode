04037631971bfaba2736b4199d4e10d589963c93
Lambert Kuang <lambertkuang@gmail.com>
2024-09-02 14:24:34 -0700

    Initialize BFS with all rotting oranges

--- graph/rotting_oranges.py
+++ graph/rotting_oranges.py
@@ -43,92 +43,42 @@ grid[i][j] is 0, 1, or 2.
 
 class Solution(TestCase):
     def orangesRotting(self, grid: list[list[int]]) -> int:
-        # go through each element in grid
-        # find min distance to rotting orange from fresh oranges
-        # return -1 if not possible
-        self.grid = grid
-        self.time_grid = deepcopy(self.grid)
-        self.visited_grid = set()
-        rows = len(grid)
-        cols = len(grid[0])
-        max_time_to_rot = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    self.fill_time_grid(r, c)
-                # if grid[r][c] == 1:
-                #     time_to_rot = self.get_time_to_rot(r, c)
-                #     if time_to_rot == -1:
-                #         return -1
-                #     max_time_to_rot = max(max_time_to_rot, time_to_rot)
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1 and (r, c) not in self.visited_grid:
-                    return -1
-                if (r, c) in self.visited_grid:
-                    max_time_to_rot = max(max_time_to_rot, self.time_grid[r][c])
-
-        return max_time_to_rot
-
-    def fill_time_grid(self, r: int, c: int) -> None:
-        visited = set()
+        # start from all rotten oranges
+        # enqueue all rotten oranges, BFS from there
         q = deque()
-        q.append((r, c))
+        visited = set()
         directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
+        total_fresh = 0
         time_taken = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    total_fresh += 1
+                if grid[row][col] == 2:
+                    q.append((row, col))
+
         while q:
+            print(q)
             for _ in range(len(q)):
                 row, col = q.popleft()
-                visited.add((row, col))
                 for dir in directions:
                     nr = row + dir[0]
                     nc = col + dir[1]
                     if (
                         nr >= 0
-                        and nr < len(self.grid)
+                        and nr < len(grid)
                         and nc >= 0
-                        and nc < len(self.grid[0])
+                        and nc < len(grid[0])
                         and (nr, nc) not in visited
-                        and self.grid[nr][nc] == 1
+                        and grid[nr][nc] == 1
                     ):
-                        if (nr, nc) in self.visited_grid:
-                            self.time_grid[nr][nc] = min(self.time_grid[nr][nc], time_taken + 1)
-                        else:
-                            self.time_grid[nr][nc] = time_taken + 1
-                        self.visited_grid.add((nr, nc))
+                        visited.add((nr, nc))
+                        total_fresh -= 1
                         q.append((nr, nc))
             time_taken += 1
-
-
-    def get_time_to_rot(self, r: int, c: int) -> int:
-        visited = set()
-        q = deque()
-        q.append((r, c))
-        time = 0
-        directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
-        while q:
-            for _ in range(len(q)):
-                row, col = q.popleft()
-                for dir in directions:
-                    nr = row + dir[0]
-                    nc = col + dir[1]
-                    is_in_range = (
-                        nr >= 0
-                        and nr < len(self.grid)
-                        and nc >= 0
-                        and nc < len(self.grid[0])
-                    )
-                    if (
-                        is_in_range
-                        and (nr, nc) not in visited
-                        and self.grid[nr][nc] == 1
-                    ):
-                        q.append((nr, nc))
-                    elif is_in_range and self.grid[nr][nc] == 2:
-                        return time + 1
-            time += 1
-        return -1
+        if total_fresh > 0:
+            return -1
+        return max(time_taken - 1, 0)
 
     def method(self):
         return self.orangesRotting
@@ -138,6 +88,9 @@ class Solution(TestCase):
             ([[2,1,1],[1,1,0],[0,1,1]], 4),
             ([[2,1,1],[0,1,1],[1,0,1]], -1),
             ([[0,2]], 0),
+            ([[2,1,1],[0,1,1],[1,0,1]], -1),
+            ([[0]], 0),
+            ([[2,2],[1,1],[0,0],[2,0]], 1),
         ]
 
 Solution().run_tests()
