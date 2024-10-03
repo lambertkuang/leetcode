ac15b5b1209496f9882e8a10051cea7bed033c8a
Lambert Kuang <lambertkuang@gmail.com>
2024-10-03 16:24:24 -0700

    Simplify n queens

--- backtracking/n_queens.py
+++ backtracking/n_queens.py
@@ -32,85 +32,46 @@ class Solution(TestCase):
     def solveNQueens(self, n: int) -> list[list[str]]:
         solutions = []
         board = [['.' for _ in range(n)] for _ in range(n)]
+        cols = set()
+        pos_diagonal = set()
+        neg_diagonal = set()
 
-        def backtrack(row: int, cur: list[list[str]], remaining: int) -> None:
-            if remaining + row > n or not self.isValidBoard(cur.copy()):
-                return
-
-            if remaining == 0 and self.isValidBoard(cur.copy()):
+        def backtrack(row: int, cur: list[list[str]]) -> None:
+            if row == n:
                 solutions.append([''.join(r) for r in cur.copy()])
                 return
 
-            for r in range(row, n):
-                for c in range(n):
-                    cur[r][c] = 'Q'
-                    backtrack(r + 1, cur, remaining - 1)
-                    cur[r][c] = '.'
+            for col in range(n):
+                if (
+                    col in cols
+                    or (row + col) in pos_diagonal
+                    or (row - col) in neg_diagonal
+                ):
+                    continue
+                cur[row][col] = 'Q'
+                cols.add(col)
+                pos_diagonal.add(row + col)
+                neg_diagonal.add(row - col)
 
-        backtrack(0, board, n)
+                backtrack(row + 1, cur)
+
+                cur[row][col] = '.'
+                cols.remove(col)
+                pos_diagonal.remove(row + col)
+                neg_diagonal.remove(row - col)
+
+        backtrack(0, board)
+
         return solutions
-
-    def isValidBoard(self, board: list[list[str]]) -> bool:
-        # check that each row, col, and diagonal can have at most one queen
-        n = len(board)
-        # checking rows
-        for row in board:
-            has_queen = False
-            for cell in row:
-                if cell == 'Q':
-                    if has_queen:
-                        return False
-                    has_queen = True
-
-        # checking columns
-        for c in range(n):
-            has_queen = False
-            for r in range(n):
-                if board[r][c] == 'Q':
-                    if has_queen:
-                        return False
-                    has_queen = True
-
-        # checking diagonals
-        for r in range(n):
-            for c in range(n):
-                left = self.checkLeftDiagonal(r, c, board)
-                right = self.checkRightDiagonal(r, c, board)
-                if not (left and right):
-                    return False
-
-        return True
-
-    def checkLeftDiagonal(self, row: int, col: int, board: list[list[str]]) -> bool:
-        has_queen = False
-        while row < len(board) and col >= 0:
-            if board[row][col] == 'Q':
-                if has_queen:
-                    return False
-                has_queen = True
-            row += 1
-            col -= 1
-        return True
-
-    def checkRightDiagonal(self, row: int, col: int, board: list[list[str]]) -> bool:
-        has_queen = False
-        while row < len(board) and col < len(board):
-            if board[row][col] == 'Q':
-                if has_queen:
-                    return False
-                has_queen = True
-            row += 1
-            col += 1
-        return True
 
     def method(self):
         return self.solveNQueens
 
     def test_cases(self):
         return [
-            # (1, [["Q"]]),
-            # (2, []),
-            # (3, []),
+            (1, [["Q"]]),
+            (2, []),
+            (3, []),
             (4, [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]),
         ]
 
